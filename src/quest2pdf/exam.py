315999cd35e171ecebf2587e2b0c7198716dd1f6
Giancarlo Ossino <agossino@iperbole.bo.it>
2020-01-01 23:45:29 +0100

    developing Answer Question

--- src/quest2pdf/exam.py
+++ src/quest2pdf/exam.py
@@ -1,173 +1,52 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from reportlab.lib.pagesizes import  A4
-from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
-from reportlab.lib.units import mm
 from pathlib import Path
-from datetime import datetime
-import os
-import platform
+from typing import Set
 import logging
 
-from multiquest import MultiQuest
-from numberedcanvas import NumberedCanvas
 
 LOGNAME = 'quest2pdf.' + __name__
 LOGGER = logging.getLogger(LOGNAME)
 
-class ExamDoc:
-    def __init__(self, quests, nDoc=1,
-                 exam_filename='Exam',
-                 correction_filename='Correction',
-                 destination=None,
-                 to_shuffle=False,
-                 heading=False):
 
-        author = os.getlogin() + '@' + platform.node()
-        title = exam_filename
-        subject = 'Formazione'
+class Answer:
+    def __init__(self, text: str = ""):
+        self.text: str = text
+        self.is_correct: bool = True
+        self.image: Path = Path(".")
 
-        correction_file = Path(correction_filename)
+    @property
+    def text(self):
+        return self._text
 
-        now = datetime.now().strftime('%Y-%m-%d-T%H-%M-%S-%f')
-        correction_filename = ''.join((correction_file.stem, '-', now)) + '.pdf'
-        correction_filepath = Path(destination) / correction_filename
-        self.correctionDoc = SimpleDocTemplate(str(correction_filepath))
-        self.correctionText = []    
+    @text.setter
+    def text(self, text: str):
+        if isinstance(text, str):
+            self._text = text
+        else:
+            raise TypeError(f"{text} is not a string")
 
-        exam_filename = Path(exam_filename)
+    @property
+    def is_correct(self) -> bool:
+        return self._is_correct
 
-        self.examDoc = []
+    @is_correct.setter
+    def is_correct(self, value: bool):
+        if isinstance(value, bool):
+            self._is_correct = value
+        else:
+            raise TypeError(f"{value} is not a Path")
 
-        self.questions = []
+    @property
+    def image(self) -> Path:
+        return self._image
 
-        dictLst = [self._setDictionary(row) for row in quests]
-
-        self.evenHead = []
-        self.oddHead = []
-
-        for i in range(nDoc):
-            story = []
-            # %f are microseconds
-            now = datetime.now().strftime('%Y-%m-%d-T%H-%M-%S-%f')
-            exam_filename = ''.join((exam_filename.stem, '-', now)) + '.pdf'
-            exam_filepath = Path(destination) / exam_filename
-
-            if heading is False:
-                heading = ''
-            elif heading is True:
-                heading = exam_filename
-
-            self.evenHead.append(lambda d, c : self._evenHead(d, c,
-                                                            text=heading))
-            self.oddHead.append(lambda d, c : self._oddHead(d, c,
-                                                          text=heading))
-            
-            doc = SimpleDocTemplate(str(exam_filepath), pagesize=A4, allowSplitting=0,
-                                    author=author, title=title, subject=subject)
-            self.examDoc.append(doc)
-
-            self.questions.append(MultiQuest(dictLst, to_shuffle))
-
-            self._fillCorrectionFile(exam_filename)
-
-        return
-
-    def _setDictionary(self, row):
-        """Give the right format for SimpleTest argument.
-        """
-        try:
-            output = {"subject": row['subject'],
-                      'question': row['question'],
-                      'image': row['image']
-                      }
-            answerKeys = ('A', 'B', 'C', 'D') # TODO da 2 a n opzioni
-            answers = [row[key] for key in answerKeys]
-        except KeyError:
-            LOGGER.critical("Chiave non ammessa: %s", row)
-            raise
-
-        output['answers'] = answers
-        
-        return output
-
-    def _fillCorrectionFile(self, examFileName):
-        styles = getSampleStyleSheet()
-        text = examFileName + '\n' + self.questions[-1].__str__()
-        
-        for line in text.split('\n'):
-            para = Paragraph(line+'\n', styles["Normal"])
-            self.correctionText.append(para)
-
-        self.correctionText.append(Spacer(mm, mm * 20)) 
-
-        return
-
-
-    def _evenHead(self, canvas, doc, text=''):
-        # Save the state of our canvas so we can draw on it
-        canvas.saveState()
-        styles = getSampleStyleSheet()
-        header_text = text
-
-        # Header
-        header = Paragraph(header_text, styles['Normal'])
-        w, h = header.wrap(doc.width, doc.topMargin)
-        header.drawOn(canvas, doc.leftMargin, doc.height + doc.bottomMargin + doc.topMargin/2 - h)
-
-        # Release the canvas
-        canvas.restoreState()
-
-        return
-
-    def _oddHead(self, canvas, doc, text=''):
-        # Save the state of our canvas so we can draw on it
-        canvas.saveState()
-        styles = getSampleStyleSheet()
-        header_text = text
-
-        # Header
-        header = Paragraph(header_text, styles['Normal'])
-        w, h = header.wrap(doc.width, doc.topMargin)
-        header.drawOn(canvas, doc.leftMargin, doc.height + doc.bottomMargin + doc.topMargin/2 - h)
-
-        # Release the canvas
-        canvas.restoreState()
-
-        return
-
-    def _footer(self, canvas, doc):
-        # Save the state of our canvas so we can draw on it
-        canvas.saveState()
-        styles = getSampleStyleSheet()
-        text = 'This is a multi-line footer.  It goes on every page.   ' * 5
-
-        # Footer
-        footer = Paragraph(text, styles['Normal'])
-        w, h = footer.wrap(doc.width, doc.bottomMargin)
-        footer.drawOn(canvas, doc.leftMargin, h)
-
-        # Release the canvas
-        canvas.restoreState()
-
-        return
-
-    def close(self) -> bool:
-        self.correctionDoc.build(self.correctionText)
-
-        story = []
-
-        for q, doc, h1, h in zip(self.questions, self.examDoc,
-                                 self.evenHead, self.oddHead):
-            for f in q.get_flowables():
-                story.append(f)
-                story.append(Spacer(mm, mm*20))
-
-            doc.build(story, onFirstPage=h1,
-                      onLaterPages=h, canvasmaker=NumberedCanvas)
-        return True
-
+    @image.setter
+    def image(self, file_path: str):
+        if isinstance(file_path, Path):
+            self._image = file_path
+        else:
+            raise TypeError(f"{file_path} is not a Path")
 
 class Question:
     def __init__(self, text: str = ""):
@@ -175,6 +54,7 @@ class Question:
         self.image: Path = Path(".")
         self.subject: str = ""
         self.level: int = 0
+        self._answers: Set[Answer] = set()
 
     @property
     def text(self) -> str:
@@ -214,16 +94,29 @@ class Question:
         return self._level
 
     @level.setter
-    def level(self, value: int):
+    def level(self, value: int) -> None:
         if isinstance(value, int):
             self._level = value
         else:
             raise TypeError(f"{value} is not a string")
 
+    @property
+    def answers(self) -> Set[Answer]:
+        return self._answers
+
+    def add_answer(self, answer: Answer) -> None:
+        if isinstance(answer, Answer):
+            self._answers.add(answer)
+        else:
+            raise TypeError(f"{answer} is not an Answer")
+
 
 if __name__ == "__main__":
     q = Question("ciao")
-    setattr(q, "image", "x")
-    print(getattr(q, "image"))
+    a1 = Answer("That's me.")
+    a2 = Answer("That's me.")
+    q.add_answer(a2)
+    q.add_answer(a1)
+    print(q.answers)
 
 
--- tests/unit/test_exam.py
+++ tests/unit/test_exam.py
@@ -3,6 +3,50 @@ import pytest
 from pathlib import Path
 
 
+def test_answer_text1():
+    a = exam.Answer()
+    expected = ""
+
+    assert a.text == ""
+
+
+def test_answer_text2():
+    text = "Roma"
+    q = exam.Answer(text)
+
+    assert q.text == text
+
+
+@pytest.mark.parametrize(
+    "attribute, expected",
+    [("image", Path(".")),
+     ("is_correct", True)],
+)
+def test_answer_get(attribute, expected):
+    text = "Po"
+    q = exam.Answer(text)
+
+    assert getattr(q, attribute) == expected
+
+
+@pytest.mark.parametrize(
+    "attribute, expected",
+    [pytest.param("text", 0.1, marks=pytest.mark.xfail),
+     ("image", Path(r"\home")),
+     pytest.param("image", r"\home", marks=pytest.mark.xfail),
+     ("is_correct", False),
+     pytest.param("is_correct", 1_000, marks=pytest.mark.xfail)],
+)
+def test_answer_set(attribute, expected):
+    q = exam.Answer()
+    try:
+        setattr(q, attribute, expected)
+    except TypeError:
+        assert False
+
+    assert getattr(q, attribute) == expected
+
+
 def test_question_get_text1():
     q = exam.Question()
     expected = ""
@@ -21,13 +65,15 @@ def test_question_set_text2():
     "attribute, expected",
     [("image", Path(".")),
      ("subject", ""),
-     ("level", 0)],
+     ("level", 0),
+     ("answers", set())],
 )
 def test_question_get(attribute, expected):
     text = "What's your name?"
     q = exam.Question(text)
 
-    assert q.__getattribute__(attribute) == expected
+    assert getattr(q, attribute) == expected
+
 
 @pytest.mark.parametrize(
     "attribute, expected",
@@ -37,10 +83,31 @@ def test_question_get(attribute, expected):
      ("subject", "Math"),
      pytest.param("subject", 1_000, marks=pytest.mark.xfail),
      ("level", 1_000),
-     pytest.param("level", 1_000.1, marks=pytest.mark.xfail)],
+     pytest.param("level", 1_000.01, marks=pytest.mark.xfail)],
 )
 def test_question_set(attribute, expected):
     q = exam.Question()
-    setattr(q, attribute, expected)
+    try:
+        setattr(q, attribute, expected)
+    except TypeError:
+        assert False
 
     assert getattr(q, attribute) == expected
+
+
+def test_question_answer_add():
+    q = exam.Question("Who are you?")
+    a = exam.Answer("That's me.")
+    q.add_answer(a)
+
+    assert a in q.answers
+
+
+def test_question_answer_add_autoset():
+    q = exam.Question("Who are you?")
+    a1 = exam.Answer("That's me.")
+    a2 = exam.Answer("That's not me.")
+    q.add_answer(a2)
+    q.add_answer(a1)
+
+    assert a1 in q.answers
