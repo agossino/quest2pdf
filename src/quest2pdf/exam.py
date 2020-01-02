4633f4de844503675075c4743682396504510e1f
Giancarlo Ossino <agossino@iperbole.bo.it>
2020-01-02 23:09:23 +0100

    method and attribute added to Question

--- src/quest2pdf/exam.py
+++ src/quest2pdf/exam.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from pathlib import Path
-from typing import Set
+from typing import Tuple, List
 import logging
+from random import shuffle
 
 
-LOGNAME = 'quest2pdf.' + __name__
+LOGNAME = "quest2pdf." + __name__
 LOGGER = logging.getLogger(LOGNAME)
 
 
 class Answer:
     def __init__(self, text: str = ""):
         self.text: str = text
-        self.is_correct: bool = True
         self.image: Path = Path(".")
 
     @property
-    def text(self):
+    def text(self) -> str:
         return self._text
 
     @text.setter
@@ -48,13 +48,15 @@ class Answer:
         else:
             raise TypeError(f"{file_path} is not a Path")
 
+
 class Question:
     def __init__(self, text: str = ""):
         self.text: str = text
         self.image: Path = Path(".")
         self.subject: str = ""
         self.level: int = 0
-        self._answers: Set[Answer] = set()
+        self._answers: List[Answer] = []
+        self._correct_answer = None
 
     @property
     def text(self) -> str:
@@ -101,22 +103,48 @@ class Question:
             raise TypeError(f"{value} is not a string")
 
     @property
-    def answers(self) -> Set[Answer]:
-        return self._answers
+    def answers(self) -> Tuple[Answer]:
+        return tuple(self._answers)
 
-    def add_answer(self, answer: Answer) -> None:
+    def add_answer(self, answer: Answer, is_correct: bool = True) -> None:
+        """Add an Answer. As side effect, correct answer is set.
+        The first answer is the correct one: successive answers
+        are set accordingly to is_correct.
+        """
         if isinstance(answer, Answer):
-            self._answers.add(answer)
+            self._correct_answer = self._set_correctness(answer, is_correct)
+            self._answers.append(answer)
         else:
             raise TypeError(f"{answer} is not an Answer")
 
+    @property
+    def correct_answer(self) -> Answer:
+        return self._correct_answer
+
+    def _set_correctness(self, answer: Answer, is_correct: bool) -> Answer:
+        if len(self._answers) == 0 or is_correct:
+            return answer
+        else:
+            return self._correct_answer
+
+    def shuffle(self) -> None:
+        shuffle(self._answers)
+        ix = self._answers.index(self._correct_answer)
+        print(ix, chr(ord("A") + ix))
+
 
 if __name__ == "__main__":
-    q = Question("ciao")
+    q = Question("Who are you?")
     a1 = Answer("That's me.")
-    a2 = Answer("That's me.")
-    q.add_answer(a2)
+    a2 = Answer("That's not me.")
+    a3 = Answer("That's him")
+    a4 = Answer("That's her.")
     q.add_answer(a1)
-    print(q.answers)
+    q.add_answer(a2)
+    q.add_answer(a3)
+    q.add_answer(a4)
+    from random import seed
 
-
+    seed(1)
+    q.shuffle()
+    q.shuffle()
--- tests/unit/test_exam.py
+++ tests/unit/test_exam.py
@@ -1,6 +1,7 @@
 import exam
 import pytest
 from pathlib import Path
+import random
 
 
 def test_answer_text1():
@@ -17,11 +18,7 @@ def test_answer_text2():
     assert q.text == text
 
 
-@pytest.mark.parametrize(
-    "attribute, expected",
-    [("image", Path(".")),
-     ("is_correct", True)],
-)
+@pytest.mark.parametrize("attribute, expected", [("image", Path("."))])
 def test_answer_get(attribute, expected):
     text = "Po"
     q = exam.Answer(text)
@@ -31,11 +28,11 @@ def test_answer_get(attribute, expected):
 
 @pytest.mark.parametrize(
     "attribute, expected",
-    [pytest.param("text", 0.1, marks=pytest.mark.xfail),
-     ("image", Path(r"\home")),
-     pytest.param("image", r"\home", marks=pytest.mark.xfail),
-     ("is_correct", False),
-     pytest.param("is_correct", 1_000, marks=pytest.mark.xfail)],
+    [
+        pytest.param("text", 0.1, marks=pytest.mark.xfail),
+        ("image", Path(r"\home")),
+        pytest.param("image", r"\home", marks=pytest.mark.xfail),
+    ],
 )
 def test_answer_set(attribute, expected):
     q = exam.Answer()
@@ -63,12 +60,17 @@ def test_question_set_text2():
 
 @pytest.mark.parametrize(
     "attribute, expected",
-    [("image", Path(".")),
-     ("subject", ""),
-     ("level", 0),
-     ("answers", set())],
+    [
+        ("image", Path(".")),
+        ("subject", ""),
+        ("level", 0),
+        ("answers", tuple()),
+        ("correct_answer", None),
+    ],
 )
 def test_question_get(attribute, expected):
+    """Test default attribute values
+    """
     text = "What's your name?"
     q = exam.Question(text)
 
@@ -77,15 +79,19 @@ def test_question_get(attribute, expected):
 
 @pytest.mark.parametrize(
     "attribute, expected",
-    [pytest.param("text", 0.1, marks=pytest.mark.xfail),
-     ("image", Path(r"\home")),
-     pytest.param("image", r"\home", marks=pytest.mark.xfail),
-     ("subject", "Math"),
-     pytest.param("subject", 1_000, marks=pytest.mark.xfail),
-     ("level", 1_000),
-     pytest.param("level", 1_000.01, marks=pytest.mark.xfail)],
+    [
+        pytest.param("text", 0.1, marks=pytest.mark.xfail),
+        ("image", Path(r"\home")),
+        pytest.param("image", r"\home", marks=pytest.mark.xfail),
+        ("subject", "Math"),
+        pytest.param("subject", 1000, marks=pytest.mark.xfail),
+        ("level", 1000),
+        pytest.param("level", 1000.01, marks=pytest.mark.xfail),
+    ],
 )
 def test_question_set(attribute, expected):
+    """Test set right and wrong attribute
+    """
     q = exam.Question()
     try:
         setattr(q, attribute, expected)
@@ -96,6 +102,8 @@ def test_question_set(attribute, expected):
 
 
 def test_question_answer_add():
+    """Test one answer addition
+    """
     q = exam.Question("Who are you?")
     a = exam.Answer("That's me.")
     q.add_answer(a)
@@ -103,11 +111,81 @@ def test_question_answer_add():
     assert a in q.answers
 
 
-def test_question_answer_add_autoset():
+def test_question_answer_correct1():
+    """Test correctness of the only
+    answer added
+    """
+    q = exam.Question("Who are you?")
+    a = exam.Answer("That's me.")
+    q.add_answer(a)
+
+    assert q.correct_answer == a
+
+
+def test_question_answer_add2():
+    """Test two answers addition
+    """
+    q = exam.Question("Who are you?")
+    a1 = exam.Answer("That's me.")
+    a2 = exam.Answer("That's not me.")
+    q.add_answer(a1)
+    q.add_answer(a2)
+
+    assert a1 in q.answers
+    assert a2 in q.answers
+    assert q.answers == (a1, a2)
+
+
+def test_question_answer_correct2():
+    """Test correctness of the last
+    answer added
+    """
+    q = exam.Question("Who are you?")
+    a1 = exam.Answer("That's me.")
+    a2 = exam.Answer("That's not me.")
+    q.add_answer(a1)
+    q.add_answer(a2)
+
+    assert q.correct_answer == a2
+
+
+def test_question_answer_correct3():
+    """Test correctness of the first
+    answer added when the successive
+    is wrong
+    """
     q = exam.Question("Who are you?")
     a1 = exam.Answer("That's me.")
     a2 = exam.Answer("That's not me.")
     q.add_answer(a2)
-    q.add_answer(a1)
+    q.add_answer(a1, False)
 
-    assert a1 in q.answers
+    assert q.correct_answer == a2
+
+
+def test_question_answer_correct4():
+    """Test ineffectiveness of correct setting
+    for the first answer added
+    """
+    q = exam.Question("Who are you?")
+    a1 = exam.Answer("That's me.")
+    q.add_answer(a1, False)
+
+    assert q.correct_answer == a1
+
+
+def test_question_shuffle():
+    q = exam.Question("Who are you?")
+    a1 = exam.Answer("That's me.")
+    a2 = exam.Answer("That's not me.")
+    a3 = exam.Answer("That's him")
+    a4 = exam.Answer("That's her.")
+    q.add_answer(a1)
+    q.add_answer(a2)
+    q.add_answer(a3)
+    q.add_answer(a4)
+
+    assert q.answers == (a1, a2, a3, a4)
+    random.seed(1)
+    q.shuffle()
+    assert q.answers == (a4, a1, a3, a2)
