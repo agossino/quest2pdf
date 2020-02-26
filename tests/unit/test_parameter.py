15d4fc5c46f57ec00988db99aa9348eac3949c84
Giancarlo Ossino <agossino@iperbole.bo.it>
2020-02-26 23:22:49 +0100

    testing logging conf in parameter

--- tests/unit/test_parameter.py
+++ tests/unit/test_parameter.py
@@ -5,6 +5,7 @@ from unit_helper import save_log_configuration
 
 def test_default(caplog):
     """test no arguments on command line
+    amd no logging configuration  file found
     """
     parsed = parameter.param_parser([])
     expected = {"input": "questions.csv",
@@ -19,36 +20,60 @@ def test_default(caplog):
                 "shuffle": False}
 
     assert parsed == expected
-    assert caplog.record_tuples[0][1] == logging.INFO
+    assert caplog.record_tuples[3][1] == logging.WARNING
 
 
-def test_cli_set1(tmp_path, caplog):
-    """test """
+def test_cli_set1(tmp_path, caplog, monkeypatch):
+    """test logging configuration file in current dir
+    """
     input_arg = "my_questions.csv"
-    log_configuration_file = tmp_path / "loggingConf.json"
+    log_configuration_file = "loggingConf.json"
     expected = {"input": input_arg,
                 "number": 1,
                 "exam": "Exam",
                 "correction": "Correction",
                 "app_configuration_file": "conf.ini",
-                "log_configuration_file": str(log_configuration_file),
+                "log_configuration_file": log_configuration_file,
                 "page_heading": False,
                 "encoding": "utf-8",
                 "delimiter": ",",
                 "shuffle": False}
-    log_configuration_file = tmp_path / expected["log_configuration_file"]
-    save_log_configuration(log_configuration_file)
+    monkeypatch.chdir(tmp_path)
+    save_log_configuration(tmp_path / log_configuration_file)
     parsed = parameter.param_parser([input_arg,
                                      "--log_configuration_file",
-                                     str(log_configuration_file)])
+                                     log_configuration_file])
 
     assert parsed == expected
-    assert caplog.record_tuples == []
-
-    caplog.clear()
 
 
-def test_cli_set2():
+def test_cli_set3(tmp_path, caplog, monkeypatch):
+    """test logging configuration file in HOME dir
+    """
+    input_arg = "my_questions.csv"
+    log_configuration_file = "loggingConf.json"
+    expected = {"input": input_arg,
+                "number": 1,
+                "exam": "Exam",
+                "correction": "Correction",
+                "app_configuration_file": "conf.ini",
+                "log_configuration_file": log_configuration_file,
+                "page_heading": False,
+                "encoding": "utf-8",
+                "delimiter": ",",
+                "shuffle": False}
+    monkeypatch.setenv("HOME", str(tmp_path))
+    save_log_configuration(tmp_path / log_configuration_file)
+    parsed = parameter.param_parser([input_arg,
+                                     "--log_configuration_file",
+                                     log_configuration_file])
+
+    assert parsed == expected
+    assert caplog.record_tuples[0][1] == logging.INFO
+    assert caplog.record_tuples[1][1] == logging.INFO
+
+
+def test_cli_set4():
     option = "--correction"
     param = "my_correction"
     parsed = parameter.param_parser([option, param])
